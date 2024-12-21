26c644d3a2b9eb7f83fcfc51826890fd34aae892
Saathwika2005 <161912995+Saathwika2005@users.noreply.github.com>
2024-12-21 13:11:05 +0530

    Update ob.py

--- ob.py
+++ ob.py
@@ -3,5 +3,5 @@ from ultralytics import YOLO
 # Load a pretrained YOLOv5 model with maximum weights
 model = YOLO("yolov5x.pt")  # yolov5x is the largest YOLOv5 model
 
-# Run inference on 'bus.jpg' with arguments
+
 model.predict(0, save=True, imgsz=320, conf=0.5,vid_stride=1)
